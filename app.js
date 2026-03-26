b3bac9e9c31726f50723893707e404a48d03cd4b
Sairaj Fulsundar <sairajfulsundar111@gmail.com>
2026-03-26 23:15:51 +0530

    fixed port

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 
+// Port (important for flexibility)
+const PORT = process.env.PORT || 3000;
+
+// Route
 app.get("/", (req, res) => {
-  res.send("Hello World from NodeJs Application");
+  res.send("Hello World from Sairaj 🚀");
 });
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+// Start server (IMPORTANT: 0.0.0.0 for EC2 access)
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
